834d8c4a432d5c154928735f0ce194c8e90b611e
tingwei0501 <tingwei0501@gmail.com>
2019-02-15 00:44:32 +0800

    edit contact list, add questionnaire

--- contactList.js
+++ contactList.js
@@ -4,10 +4,23 @@ import {
     StyleSheet,
     Text,
     FlatList,
+    TextInput,
     TouchableOpacity,
+    TouchableHighlight,
+    Picker,
+    Modal,
+    Dimensions,
+    Switch,
 } from 'react-native'
 import { vw, vh } from 'react-native-expo-viewport-units'
+import PageControl from 'react-native-page-control'
+import Carousel from 'react-native-snap-carousel'
+// import { TextInput } from 'react-native-gesture-handler'
+
 import NetUtil from './netUtil'
+import ButtonSample from './buttonSample'
+
+const { width } = Dimensions.get("window")
 
 const darkGreen = '#657359'
 const lightGreen = '#9AA582'
@@ -21,8 +34,22 @@ export default class ContactListScreen extends Component {
         this.state = {
             data: [],
             loaded: false,
+            modalVisible: false,
+            usingPurpose: '',
+            isSending: false,
+            currentPage: 0,
+            switchValue: false,
+            switchText: '沒有',
         }
         this.fetchData = this.fetchData.bind(this)
+    }
+    _updateUsingPurpose = (p) => {
+        this.setState({ usingPurpose: p })
+    }
+    _updateIsSending = (s) => {
+        console.log(s)
+        this.setState({ switchValue:s,
+                        switchText: s? '有': '沒有' })
     }
     componentDidMount() {
         const { navigation } = this.props
@@ -65,17 +92,120 @@ export default class ContactListScreen extends Component {
             </View>
         )
     }
+    setModalVisible(visible) {
+        this.setState({modalVisible: visible})
+    }
+    _onPressButton = (e) => {
+        this.setModalVisible(!this.state.modalVisible)
+    }
+    _renderTextInput() {
+        return (
+            <View>
+                <TextInput 
+                    placeholder = '其他原因'
+                    placeholderTextColor = {brown}
+                    selectionColor = {darkGreen}
+                    underlineColorAndroid = {lightPink}
+                    style = {styles.input}/>
+            </View>
+        )
+    }
+    _changeIndex = (index) => {
+        this.setState({ currentPage: index })
+    }
+    _onSelect = ( item ) => {
+        console.log(item);
+    }
     render() {
         if(!this.state.loaded) {
             return this.renderLoadingView();
         }
+        let Q1 = 
+        <View style = {styles.questionContainer}>
+            <Text style = {styles.subTitle}>1. 請問您使用此App的目的是?</Text>
+            <Picker
+                selectedValue = {this.state.usingPurpose}
+                onValueChange = {this._updateUsingPurpose}
+                style = {styles.purposePicker}>
+                    <Picker.Item label = "要和對方溝通" value = "contact" />
+                    <Picker.Item label = "純粹好奇對方狀態" value = "curious" />
+                    <Picker.Item label = "無聊打發時間" value = "boring" />
+                    <Picker.Item label = "其他" value = "other" />
+            </Picker>
+            <View>
+                {
+                    this.state.usingPurpose == 'other'
+                    ? this._renderTextInput()
+                    : <View/>
+                }
+            </View>
+        </View>
+        let Q2 = 
+        <View style = {styles.questionContainer}>
+            <Text style = {styles.subTitle}>2. 請問您看完對方狀態之後，{'\n'}有傳訊息給他嗎?</Text>
+            <View style = {{flexDirection: 'row', marginTop: 10,}}>
+                <Text>{this.state.switchText}</Text>
+                <Switch
+                    thumbColor = {lightPink}
+                    value = {this.state.switchValue}
+                    onValueChange = {this._updateIsSending}/>
+            </View>
+        </View>
+        let Q3 = 
+        <View style = {styles.questionContainer}>
+            <TouchableHighlight
+                onPress={() => {
+                    this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Text>關閉問卷</Text>
+            </TouchableHighlight>
+        </View>
+        let stepComponents = [Q1, Q2, Q3]
         return(
             <View style = {styles.container}>
+                <Modal
+                    animationType = 'fade'
+                    visible = {this.state.modalVisible}
+                    transparent = {true}
+                    onRequestClose={() => {
+                        alert('modal closed.')
+                        this.setModalVisible(false)
+                    }}>
+                    <View style = {styles.modalContainer}>
+                        <View style = {styles.modal}>
+                            <Text style = {styles.title}>問卷</Text>
+                            <View>
+                                <Carousel
+                                    ref = {(c) => { this._carousel = c; }}
+                                    data = {stepComponents}
+                                    renderItem = {({ item }) => item}
+                                    sliderWidth = {width/1.2}
+                                    itemWidth = {width/1.2}
+                                    onSnapToItem = {this._changeIndex}
+                                />
+                                <PageControl  
+                                    numberOfPages = {stepComponents.length}
+                                    currentPage = {this.state.currentPage}
+                                    hidesForSinglePage
+                                    pageIndicatorTintColor = {greyPink}
+                                    currentPageIndicatorTintColor = 'white'
+                                    indicatorStyle = {{borderRadius: 5}}
+                                    currentIndicatorStyle = {{borderRadius: 5}}
+                                    indicatorSize = {{width:8, height:8}}
+                                    onPageIndicatorPress = {this.onItemTap} />
+                            </View>
+                        </View>                        
+                    </View>
+                </Modal>
                 <FlatList
                     data = {this.state.data}
                     renderItem = {this.renderItem}
                     keyExtractor = {item => item.id}
                     style = {styles.list}/>
+                <ButtonSample 
+                    title = "問卷"
+                    onPress = {this._onPressButton.bind(this)}
+                    onPress = {e => this._onPressButton(e)}/>
             </View>
         )
     }
@@ -86,6 +216,19 @@ var styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: darkGreen,
+    },
+    modalContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    modal: {
+        backgroundColor: 'white',
+        alignItems: "center",
+        justifyContent: 'flex-start',
+        borderRadius: 15,
+        width: vw(80),
+        height: vh(50),
+        marginTop: vh(20),
     },
     loadingText: {
         color: lightPink,
@@ -104,8 +247,29 @@ var styles = StyleSheet.create({
         justifyContent: 'center', 
         alignItems: 'center',
         backgroundColor: lightPink, 
-        // borderRadius: 10, 
         overflow: 'hidden',
         margin: 0.5,
+    },
+    purposePicker: {
+        marginLeft: 10,
+        width: vw(50),
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginTop: 10,
+    },
+    subTitle: {
+        marginTop: 20,
+    },
+    input: {
+        width: vw(45),
+    },
+    questionContainer: {
+        alignItems: 'center',
+    },
+    checkBox: {
+        padding: 10,
+        // flex:1, 
     },
 })
